b3f0fb1a8afca9b3eb2b075ccc86ba37f821f733
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2019-05-14 16:46:19 +0200

    Implemented artificial data reader.

---
+++ onmt/inputters/artificial_coref.py
@@ -0,0 +1,27 @@
+import torch
+
+from onmt.inputters.datareader_base import DataReaderBase
+
+
+class ArtificialCorefDataReader(DataReaderBase):
+    def __init__(self, size):
+        super(ArtificialCorefDataReader, self).__init__()
+        self.size = size
+        self.tok_before = 5
+        self.tok_after = 5
+
+    @classmethod
+    def from_opt(cls, opt):
+        return cls(opt.artificial_size)
+
+    def read(self, sequences, side, _dir=None):
+        for i, s in enumerate(snt.decode('utf-8').rstrip('\n').split(' ') for snt in sequences):
+            coref_per_snt = []
+            for j, w in enumerate(s):
+                if w.startswith('coref'):
+                    cluster_id = int(w[5:])
+                    cluster_emb = torch.zeros(1220)
+                    cluster_emb[cluster_id] = 1
+                    coref_per_snt.append(((j, j), cluster_emb, cluster_id))
+            yield {side: (s, coref_per_snt), 'indices': i}
+
