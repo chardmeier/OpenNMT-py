3cb90d6796a4762a36aa639eb2593af3f078c9da
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2019-05-15 10:46:54 +0200

    Add position in cluster.

--- onmt/inputters/artificial_coref.py
+++ onmt/inputters/artificial_coref.py
@@ -17,14 +17,17 @@ class ArtificialCorefDataReader(DataReaderBase):
         text = [snt.decode('utf-8').rstrip('\n').split('\t')[1] for snt in sequences]
         for i, s in enumerate(snt.split(' ') for snt in text):
             coref_per_snt = []
+            pos_in_cluster = 0
             for j, w in enumerate(s):
                 if w.startswith('antecedent'):
                     cluster_id = int(w[10:])
                     cluster_emb = torch.zeros(2, 1220)
                     cluster_emb[0, cluster_id] = 1
-                    coref_per_snt.append(((j, j), cluster_emb, cluster_id))
+                    coref_per_snt.append(([((j, j), pos_in_cluster)], cluster_emb, cluster_id))
+                    pos_in_cluster += 1
                 elif w == 'anaphor':
-                    coref_per_snt.append(((j, j), cluster_emb, cluster_id))
+                    coref_per_snt.append(([((j, j), pos_in_cluster)], cluster_emb, cluster_id))
+                    pos_in_cluster += 1
 
             yield {side: (s, coref_per_snt), 'indices': i}
 
