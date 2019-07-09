0e554e969758dd2fd8accfe1bb0a9cef4706c6d8
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2019-07-09 11:28:17 +0100

    Added analyse_attention.py.

---
+++ analyse_attention.py
@@ -0,0 +1,29 @@
+import sys
+import torch
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('Usage: %s attention_log sntno' % sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+    logfile = sys.argv[1]
+    sntno = int(sys.argv[2])
+
+    src_text, gate_vals, attn_mt, attn_coref, chain_map = torch.load(logfile, map_location='cpu')
+
+    s_src = src_text[sntno]
+    s_gate = gate_vals[sntno]
+    s_attn_mt = attn_mt[sntno]
+    s_attn_ctx = attn_coref[chain_map == sntno]
+
+    print('Sentence %d' % sntno)
+    print('%d words' % len(s_src))
+    print('%d active chains' % s_attn_ctx.shape[0])
+    print(s_src)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
+
