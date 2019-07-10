c60186859af3ab4dd8257a0de72db2303c0ebb86
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2019-07-10 11:21:19 +0100

    Gather more information.

--- analyse_attention.py
+++ analyse_attention.py
@@ -1,5 +1,9 @@
+import collections
 import sys
 import torch
+
+
+Cluster = collections.namedtuple('Cluster', ['cluster_id', 'spans', 'gate', 'attention'])
 
 
 def main():
@@ -13,8 +17,9 @@ def main():
         torch.load(logfile, map_location='cpu')
 
     s_src = src_text[sntno]
+    sntlen = len(s_src)
     s_context_doc = context_docs[sntno]
-    s_gate = gate_vals[sntno]
+    s_gate = gate_vals[sntno, :sntlen, :]
     s_attn_mt = attn_mt[sntno]
     s_attn_ctx = attn_coref[chain_map == sntno]
     s_cluster_ids = chain_id[chain_map == sntno]
@@ -24,9 +29,14 @@ def main():
     print('%d active chains' % s_attn_ctx.shape[0])
     print(s_src)
 
-    for i in s_cluster_ids:
-        print('\nChain %d' % i.item())
-        print(s_context_doc.coref_pred['clusters'][i])
+    clusters = []
+    for i, c in enumerate(s_cluster_ids):
+        spans = s_context_doc.coref_pred['clusters'][c]
+        attn = s_attn_ctx[i, :, :sntlen, :len(spans)]
+        clusters.append(Cluster(i, spans, s_gate, attn))
+
+    for c in clusters:
+        print(c)
 
     return
 
