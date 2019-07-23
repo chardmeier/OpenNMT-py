094a2fc7fae1585c3f322ba2a9926a142d0b891b
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2019-07-23 17:51:55 +0100

    Implemented training.

--- attn_to_wa.py
+++ attn_to_wa.py
@@ -1,5 +1,8 @@
+import argparse
+import sys
 import torch
 import torch.nn
+import torch.optim
 
 
 class AttentionToWordAlignment(torch.nn.Module):
@@ -17,13 +20,52 @@ class AttentionToWordAlignment(torch.nn.Module):
         return self.conv(cnvin).squeeze(1)
 
 
+def alignment_matrices(srcf, tgtf, aligf):
+    for s, t, a in zip(srcf, tgtf, aligf):
+        srclen = len(s.split(' '))
+        tgtlen = len(t.split(' '))
+        matrix = torch.zeros(srclen, tgtlen)
+        for ap in a.rstrip('\n').split(' '):
+            si, ti = (int(x) for x in ap.split('-'))
+            matrix[si, ti] = 1
+        yield matrix
+
+
 def main():
-    sizes = [(10, 10), (4, 4), (13, 11), (1, 1)]
-    a2wa = AttentionToWordAlignment(6, 8)
-    for nx, ny in sizes:
-        inp = torch.rand(6, 8, nx, ny).unsqueeze(0)
-        out = a2wa(inp)
-        print(inp.shape, '->', out.shape)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-attn', required=True, help='NMT attentions')
+    parser.add_argument('-src', required=True, help='Tokenised source corpus')
+    parser.add_argument('-tgt', required=True, help='Tokenised target corpus')
+    parser.add_argument('-alig', required=True, help='Word alignments')
+    parser.add_argument('-save_model', required=True, help='File name to save model to')
+    parser.add_argument('-epochs', default=100, help='Number of epochs to train')
+    parser.add_argument('-nlayers', type=int, default=6, help='Number of Transformer layers')
+    parser.add_argument('-nheads', type=int, default=8, help='Number of attention heads per layer')
+    parser.add_argument('-lr', type=float, default=.001, help='Learning rate')
+    parser.add_argument('-momentum', type=float, default=.9, help='Momentum')
+    args = parser.parse_args()
+
+    attn = torch.load(args.attn)
+
+    a2wa = AttentionToWordAlignment(args.nlayers, args.nheads)
+    optim = torch.optim.SGD(a2wa.parameters(), lr=args.lr, momentum=args.momentum)
+    criterion = torch.nn.BCEWithLogitsLoss()
+
+    with open(args.src, 'r') as srcf, open(args.tgt, 'r') as tgtf, open(args.alig, 'r') as aligf:
+        for epoch in range(args.epochs):
+            running_loss = 0
+            for attn_mat, alig_mat in zip(attn, alignment_matrices(srcf, tgtf, aligf)):
+                optim.zero_grad()
+                pred = a2wa(attn_mat)
+                loss = criterion(pred, alig_mat)
+                loss.backward()
+                optim.step()
+
+                running_loss += loss.item()
+
+            print('Epoch %d: Loss = %g' % (epoch, running_loss / len(attn)), file=sys.stderr)
+
+    torch.save(a2wa.state_dict(), args.save_model)
 
 
 if __name__ == '__main__':
