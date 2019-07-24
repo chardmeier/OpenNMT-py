6f9bba554c8a0dd9ff161a8e7e01839079d366ce
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2019-07-24 14:46:45 +0100

    Bugfix.

--- dump_ds_for_alignment.py
+++ dump_ds_for_alignment.py
@@ -6,10 +6,10 @@ def main():
     for fname in sys.argv[1:]:
         ds = torch.load(fname)
         for x in ds.examples:
-            src = [w.encode('unicode_escape') for w in x.src]
+            src = [w.encode('unicode_escape') for w in x.src[0]]
             print(b' '.join(src).decode('ascii'), file=sys.stdout)
 
-            tgt = [w.encode('unicode_escape') for w in x.tgt]
+            tgt = [w.encode('unicode_escape') for w in x.tgt[0]]
             print(b' '.join(tgt).decode('ascii'), file=sys.stderr)
 
 
